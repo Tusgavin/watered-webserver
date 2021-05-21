e57f6104116d5af1c6ecc09680d7e99ed9965951
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-05-20 21:08:40 -0300

    remove firstName, lastName and birthdate from validation

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -4,11 +4,8 @@ module.exports = {
    signupValidation: async (requestFields) => {
       const signupSchema = yup.object().shape({
          username: yup.string().required(),
-         firstName: yup.string().required(),
-         lastName: yup.string(),
          password: yup.string().required(),
          email: yup.string().required(),
-         birthdate: yup.date().required(),
       });
 
       await signupSchema.validate(requestFields, {
